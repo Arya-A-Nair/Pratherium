7b60b1204feb69074c70b7d590738b3048c1cc98
Arya Nair <aryaajitnair@gmail.com>
2022-01-22 02:32:29 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,10 @@
+card_text=document.getElementById("card-text");
+card_text.innerHTML ="I am launching a revolutionary coin. Which is going to be the future of cryptocurrency. It combines the concept of traditional shady transaction with cryptocurrency";
+
+function success(){
+    disclaimer=document.getElementById("disclaimer");
+    disclaimer.style.display="none";
+    content=document.getElementById("content");
+    content.style.display="block";
+    document.body.classList.remove("bg-dark");
+}
